9cd5a24bec0cb126454ffe146efad110283ebd4c
Oleg Skydan <olegskidan@yahoo.com>
2023-04-27 10:15:54 +0200

    Allow /ping to have no authentication

--- backend/src/main/java/com/skydan/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/skydan/security/SecurityFilterChainConfig.java
@@ -42,6 +42,8 @@ public class SecurityFilterChainConfig {
                         "/api/v1/auth/login"
                 )
                 .permitAll()
+                .requestMatchers(HttpMethod.GET, "/ping")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
